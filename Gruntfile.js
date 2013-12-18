a9f3e79ab035ff1cf722c9788568bfa9e75757a3
Hengkiardo <hengki.sihombing@gmail.com>
2013-12-18 18:40:52 +0700

    recess

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,17 +11,37 @@ module.exports = function(grunt) {
       basic_and_extras: {
         files: {
           'js/plugins.js': ['js/**/*.js', 'bower/bootstrap/js/*.js'],
-          'js/main.js': ['js/frontend/main.js']
+          'js/main.js': ['js/main.js']
         }
       }
     },
     clean: {
       dev: ["js/plugins.js"]
     },
+    recess: {
+      dev: {
+        options: {
+          compile: true,
+        },
+        files: {
+          'css/style.css': ['css/style.less']
+        }
+      },
+      build: {
+        options: {
+          compile: true,
+          compress: true
+        },
+        files: {
+          'css/style.css': ['css/style.less']
+        }
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-recess');
 
-  grunt.registerTask('dev', ['clean', 'concat']);
+  grunt.registerTask('dev', ['clean', 'concat', 'recess:dev']);
 };
